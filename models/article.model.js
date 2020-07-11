6fa904dda7758291e101818e775573b01efd4a6e
Moses <netshitanganimoses@gmail.com>
2020-07-11 16:12:06 +0200

    new date format

--- models/article.model.js
+++ models/article.model.js
@@ -4,10 +4,9 @@ const mongoose = require('mongoose');
 const articleSchema = new mongoose.Schema(
     {
         author: {type: String, required: true},
-        date: {type: Date, required: true},
+        date: {type: String, required: true},
         title: {type: String, required: true},
         category: {type: String, required: true},
-        brief: {type: String, required: true},
         content: {type: String, required: true}
         // image type
     },
