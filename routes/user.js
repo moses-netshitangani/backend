3b330da17cb972e72eca956223d5ef84b0256397
Moses <netshitanganimoses@gmail.com>
2021-03-06 20:44:14 +0200

    removed add admin route

--- routes/user.js
+++ routes/user.js
@@ -2,14 +2,14 @@ const router = require('express').Router();
 let User = require('../models/user.model');
 
 // add admin
-router.route('/add').post((req, res) => {
-    const user = new User(req.body);
+// router.route('/add').post((req, res) => {
+//     const user = new User(req.body);
 
-    user.save()
-        .then(user => res.json(user))
-        .then(console.log('Admin succesfully added.'))
-        .catch(err => res.status(400).json("Error tryna save admin") + err);
-})
+//     user.save()
+//         .then(user => res.json(user))
+//         .then(console.log('Admin succesfully added.'))
+//         .catch(err => res.status(400).json("Error tryna save admin") + err);
+// })
 
 // retrieve admin
 router.route('/').get((req, res) => {
