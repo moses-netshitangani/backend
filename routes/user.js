15bd15dcbe7499adc92170f3b35f01dccaa0e33b
Moses <netshitanganimoses@gmail.com>
2021-03-21 00:40:54 +0200

    admin password encryption

--- routes/user.js
+++ routes/user.js
@@ -1,15 +1,31 @@
 const router = require('express').Router();
+const bcrypt = require('bcrypt');
 let User = require('../models/user.model');
 
 // add admin
-// router.route('/add').post((req, res) => {
-//     const user = new User(req.body);
+router.route('/add').post(async (req, res) => {
+    // password encryption
+    try {
+        const salt = await bcrypt.genSalt();
+        const hashedP = await bcrypt.hash(req.body.password, salt);
 
-//     user.save()
-//         .then(user => res.json(user))
-//         .then(console.log('Admin succesfully added.'))
-//         .catch(err => res.status(400).json("Error tryna save admin") + err);
-// })
+        const userObject = {
+            username: req.body.username,
+            password: hashedP
+        }
+        const user = new User(userObject);
+        console.log(`user object: ${user}`);
+
+        user.save()
+            .then(user => res.json(user))
+            .then(console.log('Admin succesfully added.'))
+            .catch(err => res.status(400).json("Error tryna save admin") + err);
+    } catch {
+        res.status(500).send();
+    }
+
+    
+})
 
 // retrieve admin
 router.route('/').get((req, res) => {
