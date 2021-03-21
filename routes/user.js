2ce944e4414f5fe7101388c825d1afb46b30db76
Moses <netshitanganimoses@gmail.com>
2021-03-21 15:07:49 +0200

    admin router removal

--- routes/user.js
+++ routes/user.js
@@ -3,29 +3,29 @@ const bcrypt = require('bcrypt');
 let User = require('../models/user.model');
 
 // add admin
-router.route('/add').post(async (req, res) => {
-    // password encryption
-    try {
-        const salt = await bcrypt.genSalt();
-        const hashedP = await bcrypt.hash(req.body.password, salt);
+// router.route('/add').post(async (req, res) => {
+//     // password encryption
+//     try {
+//         const salt = await bcrypt.genSalt();
+//         const hashedP = await bcrypt.hash(req.body.password, salt);
 
-        const userObject = {
-            username: req.body.username,
-            password: hashedP
-        }
-        const user = new User(userObject);
-        console.log(`user object: ${user}`);
+//         const userObject = {
+//             username: req.body.username,
+//             password: hashedP
+//         }
+//         const user = new User(userObject);
+//         console.log(`user object: ${user}`);
 
-        user.save()
-            .then(user => res.json(user))
-            .then(console.log('Admin succesfully added.'))
-            .catch(err => res.status(400).json("Error tryna save admin") + err);
-    } catch {
-        res.status(500).send();
-    }
+//         user.save()
+//             .then(user => res.json(user))
+//             .then(console.log('Admin successfully added.'))
+//             .catch(err => res.status(400).json("Error tryna save admin") + err);
+//     } catch {
+//         res.status(500).send();
+//     }
 
     
-})
+// })
 
 // retrieve admin
 router.route('/').get((req, res) => {
